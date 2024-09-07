f48605bca5d829266c101a16beab444edfd1f84c
Yevgeny Coonan <yacoonan@gmail.com>
2024-09-07 12:07:49 -0400

    feat: get things cleaned up

---
+++ python_src/main.py
@@ -0,0 +1,6 @@
+#This is the MAIN FILE for the ENTIRE REPOSITORY. To 'run' the software, this is the right file (unless you're unit testing..)
+
+#Just as a general note... this file is the 'glue' between all parts of the repository.
+#If you're looking to jump right into things, please create a new file to keep the repository organized.
+
+#Thanks!
--- rust_src/lib.rs
+++ rust_src/lib.rs
@@ -1,14 +1,17 @@
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
+//Hey! If you intend to add extra functionality to this repository with Rust, please create a new module folder.
+//Unless of course you're writting debug code and bindings between Python. You probably don't have to worry about this though... 
+
+/// Lil' test function written by the engineers over at PyO3
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
-/// A Python module implemented in Rust.
+/// Main python module for all things rust related.
 #[pymodule]
-fn software_2024_2025(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn rust_robot(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
