044fe2d95416e5faac59f02091890e42f1bc9ffb
Yevgeny Coonan <yacoonan@gmail.com>
2024-09-07 12:35:45 -0400

    feat: simulation start

--- rust_src/lib.rs
+++ rust_src/lib.rs
@@ -3,6 +3,8 @@ use pyo3::prelude::*;
 //Hey! If you intend to add extra functionality to this repository with Rust, please create a new module folder.
 //Unless of course you're writting debug code and bindings between Python. You probably don't have to worry about this though... 
 
+mod simulation;
+
 /// Lil' test function written by the engineers over at PyO3
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
