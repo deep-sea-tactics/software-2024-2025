ba27baa6a381b19e484dafb133694219bb9cac44
Yevgeny Coonan <150849367+Inotrandom@users.noreply.github.com>
2024-09-24 21:15:11 -0400

    Update install.sh

--- dev_shell_scripts/install.sh
+++ dev_shell_scripts/install.sh
@@ -3,7 +3,5 @@ python3 -m venv .venv
 source .venv bin/activate 
 rustc --version || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-echo please close and restart the terminal. then run this script again
-
 pip install maturin
 maturin develop
